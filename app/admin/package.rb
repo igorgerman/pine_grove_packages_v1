ccc3f10b3cc502ad1ae8c1df3e4f7fe27f80df91
Raghu Betina <raghu@firstdraft.com>
2017-02-10 20:31:51 +0000

    Whitelist columns in Package for ActiveAdmin

--- app/admin/package.rb
+++ app/admin/package.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Package do
 
+ permit_params :apartment_id, :recipient_name, :collected, :image, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
